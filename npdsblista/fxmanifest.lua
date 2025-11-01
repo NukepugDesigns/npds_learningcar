51b6d4bdfc6adf86ffd645c439d66c31a1cf5b99
Nukepug Designs <95311122+NukepugDesigns@users.noreply.github.com>
2025-11-01 15:05:50 +0100

    Add files via upload

---
+++ npdsblista/fxmanifest.lua
@@ -0,0 +1,27 @@
+fx_version 'adamant'
+game 'gta5'
+description 'Addon car for esx_dmv'
+author 'Nukepug Designs'
+
+files {
+    'data/**/*.meta'
+    --'data/**/*.xml',
+    --'data/**/*.dat',
+    --'data/**/*.ytyp'
+}
+
+
+data_file 'HANDLING_FILE'                   'data/**/handling*.meta'
+data_file 'VEHICLE_LAYOUTS_FILE'            'data/**/vehiclelayouts*.meta'
+data_file 'VEHICLE_METADATA_FILE'           'data/**/vehicles*.meta'
+data_file 'CARCOLS_FILE'                    'data/**/carcols*.meta'
+data_file 'VEHICLE_VARIATION_FILE'          'data/**/carvariations*.meta'
+data_file 'CONTENT_UNLOCKING_META_FILE'     'data/**/*unlocks.meta'
+data_file 'PTFXASSETINFO_FILE'              'data/**/ptfxassetinfo.meta'
+
+
+-- Changes
+-- Custom sign + roof sign
+
+-- Attributions
+-- Rockstar for making the blista
